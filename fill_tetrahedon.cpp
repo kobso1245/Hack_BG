13f015aef894af8cae526580693538fc1c99f686
kobso1245 <kobso1245@gmail.com>
2015-02-07 18:11:05 +0200

    zadacha1_revamp

--- fill_tetrahedon.cpp
+++ fill_tetrahedon.cpp
@@ -4,13 +4,11 @@ using namespace std;
 float fill_tetrahedon(int num)
 {
     float h = sqrt(num*num - (num/2.0)*(num/2.0));
-    cout<<"h= :"<<h<<"\n";
     float B = (1/2.0)*num*h;
-    cout<<"B= :"<<B<<"\n";
     float H = sqrt((num*num) - (4/9.0)*h*h);
     return B*H/3000.0;
 }
 int main()
 {
-    cout<<fill_tetrahedon(30)<<"\n";
+    cout<<"You need "<<fill_tetrahedon(100)<<" litres to fill the tetrahedon"<<"\n";
 }
