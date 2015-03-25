b4cf0d078c797024be8e49a1dce2c22b0dca9983
Kaloyan <kalo.evt@mail.bg>
2015-03-25 16:08:47 +0200

    Range changed on generate_numbers.py

--- week2/generate_numbers.py
+++ week2/generate_numbers.py
@@ -5,7 +5,7 @@ def main():
     name1, name2, cnt = sys.argv
     out_file = open(name2, "w")
     lst = []
-    for x in range(int(cnt) + 1):
+    for x in range(int(cnt)) :
         lst.append(str(randint(1, 1000)))
 
     out_file.write(" ".join(lst))
