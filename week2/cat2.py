86c06b4478587bea338ecf22815e2c7ede2b229f
Kaloyan <kalo.evt@mail.bg>
2015-03-25 14:31:22 +0200

    Before last task

---
+++ week2/cat2.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    name1, *name2 = sys.argv
+    for curr_file in name2:
+        out = open(curr_file, 'r')
+        print(out.read())
+        print()
+
+if __name__ == '__main__':
+    main()
---
+++ week2/sum_numbers.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+    name1, name2 = sys.argv
+    in_file = open(name2, 'r')
+    sum = 0
+    lst = in_file.read()
+    tmp = lst.split(' ')
+    for elem in tmp:
+        sum += int(elem)
+    print(sum)
+
+if __name__ == '__main__':
+    main()
