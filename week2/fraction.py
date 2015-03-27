9836a49a8351ead80fc9e03bd5a6d1fc25e04061
Kaloyan <kalo.evt@mail.bg>
2015-03-27 15:06:25 +0200

    Added friday's work

---
+++ week2/fraction.py
@@ -0,0 +1,59 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        a = numerator
+        b = denominator
+        den = 1
+        if a != 0:
+            den = gcd(a, b)
+        if den == 1:
+            self.numerator = numerator
+            self.denominator = denominator
+        else:
+            self.numerator = (numerator // den)
+            self.denominator = (denominator // den)
+
+    def __str__(self):
+        if self.numerator == self.denominator or self.numerator == 0:
+            return str(self.numerator)
+        else:
+            return "{} / {}".format(self.numerator, self.denominator)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __add__(self, other):
+        return Fraction(self.numerator * other.denominator +
+                        self.denominator * other.numerator,
+                        self.denominator * other.denominator)
+
+    def __eq__(self, other):
+        return (self.numerator == other.numerator and
+                self.denominator == other.denominator)
+
+    def __sub__(self, other):
+        return Fraction(self.numerator * other.denominator -
+                        self.denominator * other.numerator,
+                        self.denominator * other.denominator)
+
+    def __mul__(self, other):
+        return Fraction(self.numerator * other.numerator,
+                        self.denominator * other.denominator)
+
+
+def gcd(a, b):
+    if a > b:
+        return gcd(a-b, b)
+    if a < b:
+        return gcd(a, b - a)
+    if a == b:
+        return a
+
+if __name__ == '__main__':
+    a = Fraction(1, 2)
+    b = Fraction(2, 4)
+    print(gcd(1,2))
+    print(a == b)  # True
+
+    print(a + b)  # 1
+    print(a - b)  # 0
+    print(a * b) # 1 / 4'''
