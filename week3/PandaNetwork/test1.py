2f627e5dd311e97e4cab00864d3eee1462e2383d
Kaloyan <kalo.evt@mail.bg>
2015-06-02 11:53:02 +0300

    Done with week3

--- week3/PandaNetwork/test1.py
+++
@@ -1,5 +0,0 @@
-def tst(*arg):
-    print (arg)
-
-if __name__ == '__main__':
-    tst(1,2,3,4)
--- week3/PandaNetwork/testing_suite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while inotifywait $1 || inotifywait panda_tester.py; do
-    clear
-    python3 panda_tester.py
-done
