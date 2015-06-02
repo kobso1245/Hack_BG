97a1a4f14c15faf552e2f02740e5136836e72b76
Kaloyan <kalo.evt@mail.bg>
2015-06-02 22:24:08 +0300

    Clearing week2

--- week2/cat.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def main():
-    name1, name2 = sys.argv
-    outp = open(name2, 'r')
-    print(outp.read())
-    outp.close()
-if __name__ == '__main__':
-    main()
--- week2/tst.py
+++
@@ -1,7 +0,0 @@
-def tst1():
-    return tst2()
-def tst2():
-    return tst1()
-
-if __name__ == '__main__':
-    tst1()
